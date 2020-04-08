495b60ea547fadfd374357b12d54107a27cb505f
Olexiy <androidalexey2001@gmail.com>
2020-04-08 21:42:25 +0300

    Update script-fb.js

--- script-fb.js
+++ script-fb.js
@@ -95,6 +95,8 @@ $("img").click(() => {
         });
     }
 
-    window.open("http://www.facebook.com/dialog/send?app_id=435022673987553&link=https://i.gifer.com/origin/b2/b2f4b18aa4a9a3b65d1c78edeced27f9_w200.gif");
-
+    FB.ui({
+        method: 'send',
+        link: 'https://i.gifer.com/origin/b2/b2f4b18aa4a9a3b65d1c78edeced27f9_w200.gif'
+    });
 });
